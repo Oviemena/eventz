c078abccc16083c19a0d9fb53815b90f3b9313c6
oviemena okpowodu <okpowoduoviemena999@gmail.com>
2024-02-18 01:55:00 +0100

    added database connections

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,13 +10,33 @@ export default function Home() {
         <div className="wrapper grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
-              Host, Connect, Celebate: Your Events, Our Platform!
+              Host, Connect, Celebrate: Your Events, Our Platform!
             </h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 4,263+ mentors in the world-class companies with our global community</p>
-
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn helpful tips from 4,263+ mentors in the world-class
+              companies with our global community
+            </p>
+            <Button asChild size="lg" className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
+            </Button>
+            <Image
+              src="/assets/images/hero.png"
+              width={1000}
+              height={1000}
+              alt="hero image"
+              className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+            />
           </div>
-
         </div>
+      </section>
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold ">Trusted by <br /> Thousands of Events</h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search
+          CategoryFilter
+        </div>
+
       </section>
     </>
   );
